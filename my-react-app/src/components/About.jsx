a8753fedd1733add410ed10004bf3d0231964154
John S Weeks <steve@offthekitchen.com>
2026-04-17 17:27:43 -0600

    added photo and animatuion to About

--- my-react-app/src/components/About.jsx
+++ my-react-app/src/components/About.jsx
@@ -1,20 +1,35 @@
+import SteveImg from "/src/assets/steve.jpg";
+import React, { useEffect, useRef } from "react";
+
 function About() {
+  const ref = useRef();
+  useEffect(() => {
+    ref.current.classList.add("fade-in-slide");
+  });
+
   return (
     <div className="about">
       <h2>About</h2>
-      <div className="text-white">
-        I'm a developer/designer with years of experience in a wide variety of
-        technologies across many industries. After a career with a major IT
-        consulting firm, I took a few years to explore the life of a musician.
-        Now as a freelance web developer, I bring experience as an IT
-        professional, entrepreneurial resourcefulness and a creative spirit to
-        the table to build modern, responsive web sites. So if you're looking
-        for someone to help design, implement and maintain a web site that will
-        grow your online presence and get the attention of your clients, contact
-        me. I'd love to help you out.
+      <div className="flex flex-col lg:flex-row">
+        <img
+          src={SteveImg}
+          alt="Steve in Iceland"
+          className="max-w-1/2 h-fit block"
+        />
+        <div ref={ref} className="text-white mx-2 text-left">
+          I'm a developer/designer with years of experience in a wide variety of
+          technologies across many industries. After a career with a major IT
+          consulting firm, I took a few years to explore the life of a musician.
+          Now as a freelance web developer, I bring experience as an IT
+          professional, entrepreneurial resourcefulness and a creative spirit to
+          the table to build modern, responsive web sites. So if you're looking
+          for someone to help design, implement and maintain a web site that
+          will grow your online presence and get the attention of your clients,
+          contact me. I'd love to help you out.
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default About
+export default About;
